94c65a0475a292b92a65f2b528677152eac06118
Carlos Henrique <carloshenriquepereirasoaress@gmail.com>
2023-12-07 18:01:56 -0300

    Create the Spotify Authentication FeignClient

---
+++ src/main/java/com/my_exercise/spotify/client/LoginResponse.java
@@ -0,0 +1,26 @@
+package com.my_exercise.spotify.client;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class LoginResponse {
+
+    private String accessToken;
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
--- src/main/java/com/my_exercise/spotify/controller/AlbumController.java
+++ src/main/java/com/my_exercise/spotify/controller/AlbumController.java
@@ -1,6 +1,8 @@
 package com.my_exercise.spotify.controller;
 
+import com.my_exercise.spotify.client.AuthSpotifyClient;
 import com.my_exercise.spotify.client.GoogleClient;
+import com.my_exercise.spotify.client.LoginRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/spotify/api")
 public class AlbumController {
 
-    private final GoogleClient googleClient;
+    private final AuthSpotifyClient authSpotifyClient;
 
-    public AlbumController(GoogleClient googleClient) {
-        this.googleClient = googleClient;
+    public AlbumController(AuthSpotifyClient authSpotifyClient) {
+        this.authSpotifyClient = authSpotifyClient;
     }
 
     @GetMapping("/albums")
     public ResponseEntity<String> helloWord(){
-        return ResponseEntity.ok(googleClient.helloWord());
+        var request = new LoginRequest(
+                "client_credentials",
+                "03d97f5e81a0439a8fe692d94b32c0cc",
+                "1a9633be3a394d7e821f9d3c6b6327f6"
+        );
+        return ResponseEntity.ok(authSpotifyClient.login(request).getAccessToken());
     }
 }
