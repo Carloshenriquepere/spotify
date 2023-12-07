3f76dbbf663a39cf0f40c3d21f2644a4d937ca49
Carlos Henrique <carloshenriquepereirasoaress@gmail.com>
2023-12-07 18:26:17 -0300

    Make the result available in the API consuming Spotify data

---
+++ src/main/java/com/my_exercise/spotify/client/AlbumWrapper.java
@@ -0,0 +1,26 @@
+package com.my_exercise.spotify.client;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import java.util.List;
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class AlbumWrapper {
+
+    private List<Album> items;
+
+    public AlbumWrapper() {
+    }
+
+    public AlbumWrapper(List<Album> items) {
+        this.items = items;
+    }
+
+    public List<Album> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Album> items) {
+        this.items = items;
+    }
+}
--- src/main/java/com/my_exercise/spotify/controller/AlbumController.java
+++ src/main/java/com/my_exercise/spotify/controller/AlbumController.java
@@ -1,12 +1,12 @@
 package com.my_exercise.spotify.controller;
 
-import com.my_exercise.spotify.client.AuthSpotifyClient;
-import com.my_exercise.spotify.client.GoogleClient;
-import com.my_exercise.spotify.client.LoginRequest;
+import com.my_exercise.spotify.client.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/spotify/api")
@@ -14,17 +14,24 @@ public class AlbumController {
 
     private final AuthSpotifyClient authSpotifyClient;
 
-    public AlbumController(AuthSpotifyClient authSpotifyClient) {
+    private final AlbumSpotifyClient albumSpotifyClient;
+
+    public AlbumController(AuthSpotifyClient authSpotifyClient, AlbumSpotifyClient albumSpotifyClient) {
         this.authSpotifyClient = authSpotifyClient;
+        this.albumSpotifyClient = albumSpotifyClient;
     }
 
     @GetMapping("/albums")
-    public ResponseEntity<String> helloWord(){
+    public ResponseEntity<List<Album>> helloWord(){
         var request = new LoginRequest(
                 "client_credentials",
                 "03d97f5e81a0439a8fe692d94b32c0cc",
                 "1a9633be3a394d7e821f9d3c6b6327f6"
         );
-        return ResponseEntity.ok(authSpotifyClient.login(request).getAccessToken());
+        var token = authSpotifyClient.login(request).getAccessToken();
+
+        var response = albumSpotifyClient.getReleases("Bearer "+ token);
+
+        return ResponseEntity.ok(response.getAlbums().getItems());
     }
 }
